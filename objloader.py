105b1d088735f93fe6383440f3bb14872e25d951
devansh-dvj <devansh.dvj@gmail.com>
2021-06-02 23:39:48 +0530

    Added init file

--- objloader.py
+++ objloader.py
@@ -20,7 +20,6 @@ def MTL(filename):
         elif values[0] == 'map_Kd':
             # load the texture referred to by this declaration
             mtl[values[0]] = values[1]
-            print(mtl['map_Kd'])
             surf = pygame.image.load(mtl['map_Kd'])
             image = pygame.image.tostring(surf, 'RGBA', 1)
             ix, iy = surf.get_rect().size
