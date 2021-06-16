86fc8c25951c8952b54d7291484d25e58bee0262
devansh-dvj <devansh.dvj@gmail.com>
2021-06-17 01:17:58 +0530

    Split warp into numpy and PIL part

--- src/warping.py
+++ src/warping.py
@@ -2,9 +2,7 @@ import numpy as np
 from PIL import Image
 
 
-def warp(in_path, mv, warp_path, size):
-    I2 = np.array(Image.open(in_path))
-
+def warp(I2, mv, size):
     warped = np.empty((size, size, 4), dtype=np.uint8)
     for i in range(size):
         for j in range(size):
@@ -12,5 +10,12 @@ def warp(in_path, mv, warp_path, size):
                 warped[i][j] = I2[round(mv[i][j][1])][round(mv[i][j][0])]
             else:
                 warped[i][j] = np.array([0, 0, 0, 255], dtype=np.uint8)
+    return warped
+
+
+def warp_save(in_path, mv, warp_path, size):
+    I2 = np.array(Image.open(in_path))
+
+    warped = warp(I2, mv, size)
 
     Image.fromarray(warped).save(warp_path, 'png')
