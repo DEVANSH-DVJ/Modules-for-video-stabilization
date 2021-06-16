e6d7ee2e7439bdcb4357c878be240ae1c5ad00ff
devansh-dvj <devansh.dvj@gmail.com>
2021-06-17 00:34:12 +0530

    Fixed linting of motion

--- src/motion.py
+++ src/motion.py
@@ -8,15 +8,24 @@ def project(img2obj_map, size, modelview, projection, viewport):
     A = projection.T.dot(modelview.T)
     for i in range(size):
         for j in range(size):
-            v_ = A.dot(np.array(
-                [img2obj_map[i][j][0], img2obj_map[i][j][1], img2obj_map[i][j][2], 1.0]))
+            v_ = A.dot(
+                np.array([img2obj_map[i][j][0],
+                          img2obj_map[i][j][1],
+                          img2obj_map[i][j][2],
+                          1.0]
+                         )
+            )
             v_ = v_ / v_[3]
-            pixel = [(1.0 + v_[0]) * 0.5 * viewport[2] + viewport[0], (1.0 + v_[1])
-                     * 0.5 * viewport[3] + viewport[1], (1.0 + v_[2]) * 0.5]
+            pixel = [
+                (1.0 + v_[0]) * 0.5 * viewport[2] + viewport[0],
+                (1.0 + v_[1]) * 0.5 * viewport[3] + viewport[1],
+                (1.0 + v_[2]) * 0.5
+            ]
             x, y, z = round(pixel[0]), round(pixel[1]), pixel[2]
             if x < size and y < size and x >= 0 and y >= 0:
                 if z < 0.95:
                     img2img_map[i][j] = pixel
+
     return img2img_map
 
 
@@ -26,9 +35,12 @@ def unproject(depths, size, modelview, projection, viewport):
     A = np.linalg.inv(projection.T.dot(modelview.T))
     for i in range(size):
         for j in range(size):
-            v = np.array([2.0 * (j - viewport[0]) / viewport[2] - 1.0,
-                          2.0 * (i - viewport[1]) / viewport[3] - 1.0,
-                          2.0 * depths[i][j] - 1.0, 1.0])
+            v = np.array(
+                [2.0 * (j - viewport[0]) / viewport[2] - 1.0,
+                 2.0 * (i - viewport[1]) / viewport[3] - 1.0,
+                 2.0 * depths[i][j] - 1.0, 1.0]
+            )
             v_ = A.dot(v)
             img2obj_map[i][j] = v_[:3] / v_[3]
+
     return img2obj_map
