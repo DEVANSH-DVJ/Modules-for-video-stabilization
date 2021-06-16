6702c5ac2f3a78f64b60d8389154ecbfe5bfb802
devansh-dvj <devansh.dvj@gmail.com>
2021-06-16 14:52:21 +0530

    Renamed warped to warp in warping.py

--- src/warping.py
+++ src/warping.py
@@ -5,12 +5,12 @@ from PIL import Image
 def warp(in_path, motion, warp_path, size=512):
     I2 = np.array(Image.open(in_path))
 
-    warped = np.empty((size, size, 4), dtype=np.uint8)
+    warp = np.empty((size, size, 4), dtype=np.uint8)
     for i in range(size):
         for j in range(size):
             if motion[i][j][0] != -1:
-                warped[i][j] = I2[round(motion[i][j][1])][round(motion[i][j][0])]
+                warp[i][j] = I2[round(motion[i][j][1])][round(motion[i][j][0])]
             else:
-                warped[i][j] = np.array([0, 0, 0, 255], dtype=np.uint8)
+                warp[i][j] = np.array([0, 0, 0, 255], dtype=np.uint8)
 
-    Image.fromarray(warped).save(warp_path)
+    Image.fromarray(warp).save(warp_path)
