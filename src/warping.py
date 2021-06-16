4cf4935781ca4445a108e60d4a83d9147eaf82d1
devansh-dvj <devansh.dvj@gmail.com>
2021-06-17 01:10:31 +0530

    Fixed bug in warp func

--- src/warping.py
+++ src/warping.py
@@ -2,15 +2,15 @@ import numpy as np
 from PIL import Image
 
 
-def warp(in_path, motion, warp_path, size):
+def warp(in_path, mv, warp_path, size):
     I2 = np.array(Image.open(in_path))
 
-    warp = np.empty((size, size, 4), dtype=np.uint8)
+    warped = np.empty((size, size, 4), dtype=np.uint8)
     for i in range(size):
         for j in range(size):
-            if motion[i][j][0] != -1:
-                warp[i][j] = I2[round(motion[i][j][1])][round(motion[i][j][0])]
+            if mv[i][j][0] != -1:
+                warped[i][j] = I2[round(mv[i][j][1])][round(mv[i][j][0])]
             else:
-                warp[i][j] = np.array([0, 0, 0, 255], dtype=np.uint8)
+                warped[i][j] = np.array([0, 0, 0, 255], dtype=np.uint8)
 
-    Image.fromarray(warp).save(warp_path)
+    Image.fromarray(warped).save(warp_path, 'png')
