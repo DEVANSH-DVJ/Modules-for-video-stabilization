3ea33c49c5dd421327fcc2f69fe0bdb378eaeedb
devansh-dvj <devansh.dvj@gmail.com>
2021-06-17 14:58:53 +0530

    Removing logging from movie saver

--- src/movie.py
+++ src/movie.py
@@ -3,4 +3,4 @@ from moviepy.editor import ImageSequenceClip
 
 def movie_save(files, fps, out_file):
     clip = ImageSequenceClip(files, fps=fps)
-    clip.write_videofile(out_file)
+    clip.write_videofile(out_file, logger=None)
