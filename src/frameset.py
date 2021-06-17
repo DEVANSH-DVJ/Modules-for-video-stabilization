c3b226f098cf468a723f680449bb2e1e5d55362f
devansh-dvj <devansh.dvj@gmail.com>
2021-06-18 00:21:38 +0530

    Added parameters for frameset

---
+++ src/frameset.py
@@ -0,0 +1,8 @@
+x = {'min': -1.0, 'max': 1.0, 'sigma': 0.01}
+y = {'min': 0.0, 'max': 0.0, 'sigma': 0.01}
+z = {'min': -4.0, 'max': -4.0, 'sigma': 0}
+rx = {'min': 0.0, 'max': 0.0, 'sigma': 0.5}
+ry = {'min': -60.0, 'max': -60.0, 'sigma': 0.5}
+rz = {'min': 0.0, 'max': 0.0, 'sigma': 0.5}
+split = 100
+store = 'frameset.csv'
