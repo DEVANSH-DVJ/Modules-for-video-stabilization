a49a2646df54b287c98657bd39734e666468d725
devansh-dvj <devansh.dvj@gmail.com>
2021-06-18 02:33:39 +0530

    Added bash script for Chest and Handgun

---
+++ run.sh
@@ -0,0 +1,17 @@
+echo -e "config01"
+time python src/main.py config01
+
+echo -e "\n\nconfig02"
+time python src/main.py config02
+
+echo -e "\n\nconfig03"
+time python src/main.py config03
+
+echo -e "\n\nconfig04"
+time python src/main.py config04
+
+echo -e "\n\nconfig05"
+time python src/main.py config05
+
+echo -e "\n\nconfig06"
+time python src/main.py config06
