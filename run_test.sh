0bd21d99f4fdab5ea2e6e0538a1374c5f5892c17
devansh-dvj <devansh.dvj@gmail.com>
2021-06-22 00:51:40 +0530

    Updated test for data in repo

--- run_test.sh
+++ run_test.sh
@@ -33,27 +33,3 @@ time python src/main.py config11
 
 echo -e "\n\nconfig12"
 time python src/main.py config12
-
-echo -e "\n\nconfig13"
-time python src/main.py config13
-
-echo -e "\n\nconfig14"
-time python src/main.py config14
-
-echo -e "\n\nconfig15"
-time python src/main.py config15
-
-echo -e "\n\nconfig16"
-time python src/main.py config16
-
-echo -e "\n\nconfig17"
-time python src/main.py config17
-
-echo -e "\n\nconfig18"
-time python src/main.py config18
-
-echo -e "\n\nconfig19"
-time python src/main.py config19
-
-echo -e "\n\nconfig12"
-time python src/main.py config12
