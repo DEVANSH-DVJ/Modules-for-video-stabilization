e680af73687f1073f7346e528584b784a896a214
devansh-dvj <devansh.dvj@gmail.com>
2021-07-01 18:38:44 +0530

    Different codecs for avi and mp4 formats

--- src/movie.py
+++ src/movie.py
@@ -3,4 +3,7 @@ from moviepy.editor import ImageSequenceClip
 
 def movie_save(files, fps, out_file):
     clip = ImageSequenceClip(files, fps=fps)
-    clip.write_videofile(out_file, logger=None)
+    if out_file[-4:] == '.avi':
+        clip.write_videofile(out_file, codec='png', logger=None)
+    elif out_file[-4:] == '.mp4':
+        clip.write_videofile(out_file, codec='mpeg4', logger=None)
