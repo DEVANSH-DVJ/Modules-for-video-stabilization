2c946dc2e8de9789bf3ca86e41a4a8a8f55e4c48
devansh-dvj <devansh.dvj@gmail.com>
2021-07-01 14:15:57 +0530

    Removed previous testing

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,6 @@
 from datetime import datetime
 from pytz import timezone
 
-now = datetime.now(timezone('Asia/Kolkata'))
-print(now)
-print(dir(now))
-print(now.strftime('%Y.%m.%d %H:%M:%S.%f %Z'))
-
 
 def log(message):
     now = datetime.now(timezone('Asia/Kolkata'))
