9e9331073d8594996de1d73a90c675423785d8dc
devansh-dvj <devansh.dvj@gmail.com>
2021-07-01 14:15:32 +0530

    Added logging utils

--- src/utils.py
+++ src/utils.py
@@ -5,3 +5,9 @@ now = datetime.now(timezone('Asia/Kolkata'))
 print(now)
 print(dir(now))
 print(now.strftime('%Y.%m.%d %H:%M:%S.%f %Z'))
+
+
+def log(message):
+    now = datetime.now(timezone('Asia/Kolkata'))
+    now = now.strftime('%Y.%m.%d %H:%M:%S.%f %Z')
+    print('{}: {}'.format(now, message))
