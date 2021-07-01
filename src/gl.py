3c9406432c73f0356adb9de512b97ac6d5f16caa
devansh-dvj <devansh.dvj@gmail.com>
2021-07-01 16:27:49 +0530

    Shifted enable depth and texture to init

--- src/gl.py
+++ src/gl.py
@@ -4,6 +4,10 @@ import OpenGL.GLUT as GLUT
 
 
 def init(cam):
+    GL.glEnable(GL.GL_DEPTH_TEST)
+    GL.glEnable(GL.GL_TEXTURE_2D)
+    GL.glPixelStorei(GL.GL_PACK_ALIGNMENT, 1)
+
     GL.glMatrixMode(GL.GL_PROJECTION)
     GL.glLoadIdentity()
     GLU.gluPerspective(cam['fovy'], cam['aspect'], cam['zNear'], cam['zFar'])
@@ -18,7 +22,3 @@ def start(size, argv):
     GLUT.glutInitWindowSize(size, size)
     GLUT.glutInitWindowPosition(0, 0)
     GLUT.glutCreateWindow('Projections')
-
-    GL.glEnable(GL.GL_DEPTH_TEST)
-    GL.glEnable(GL.GL_TEXTURE_2D)
-    GL.glPixelStorei(GL.GL_PACK_ALIGNMENT, 1)
