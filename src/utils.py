194e494d1f37649d1b7f7aee16fcb7adf4ba446b
devansh-dvj <devansh.dvj@gmail.com>
2021-07-01 14:18:14 +0530

    Default debug parameter as True

--- src/utils.py
+++ src/utils.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pytz import timezone
 
 
-def log(message, debug):
+def log(message, debug=True):
     if debug:
         now = datetime.now(timezone('Asia/Kolkata'))
         now = now.strftime('%Y.%m.%d %H:%M:%S.%f %Z')
