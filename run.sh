19cf4645505a6de1bf80013232e13ce4cf553454
devansh-dvj <devansh.dvj@gmail.com>
2021-07-08 17:55:27 +0530

    Added run script

---
+++ run.sh
@@ -0,0 +1,8 @@
+python src/datagen.py data/Bridge/config.yaml
+python src/datagen.py data/Capetown/config.yaml
+python src/datagen.py data/Fort/config.yaml
+python src/datagen.py data/Map_v1/config.yaml
+python src/datagen.py data/Mine/config.yaml
+python src/datagen.py data/Pumpa/config.yaml
+python src/datagen.py data/Small\ Tropical\ Island/config.yaml
+python src/datagen.py data/Under\ The\ Sea/config.yaml
