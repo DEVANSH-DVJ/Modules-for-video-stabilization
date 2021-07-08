303064089cb6165755885a67b45ba82873d2a8ce
devansh-dvj <devansh.dvj@gmail.com>
2021-07-08 20:07:22 +0530

    Updated flow_save to handle resizing

--- src/utils.py
+++ src/utils.py
@@ -16,8 +16,22 @@ def config_load(config_file):
     return yaml.load(open(config_file), Loader=yaml.FullLoader)
 
 
-def flow_save(flow, flow_file):
-    flow.astype(np.float32).tofile(flow_file)
+def flow_save(flow, flow_path):
+    height, width, nBands = flow.shape
+    assert nBands == 2, "Number of bands = {} != 2".format(nBands)
+    u = flow[:, :, 0]
+    v = flow[:, :, 1]
+    assert u.shape == v.shape, "Invalid flow shape"
+    height, width = u.shape
+
+    with open(flow_path, 'wb') as flow_file:
+        np.array(width).astype(np.int32).tofile(flow_file)
+        np.array(height).astype(np.int32).tofile(flow_file)
+        tmp = np.zeros((height, width*nBands))
+        tmp[:, np.arange(width)*2] = u
+        tmp[:, np.arange(width)*2 + 1] = v
+        tmp.astype(np.float32).tofile(flow_file)
+
 
 
 def flow_load(flow_file):
