c15d63af88fd84987ffdb534fe5763c27b009349
devansh-dvj <devansh.dvj@gmail.com>
2021-07-15 19:30:38 +0530

    Importing init and start from gl

--- src/datagen.py
+++ src/datagen.py
@@ -13,31 +13,11 @@ from motion import project, unproject, genflow
 from movie import movie_save
 from objloader import OBJ
 from utils import log, config_load, flow_save, flag_save
+from gl import init, start
 
 projection = None
 modelview = None
 viewport = None
-
-
-def init(cam):
-    GL.glMatrixMode(GL.GL_PROJECTION)
-    GL.glLoadIdentity()
-    GLU.gluPerspective(cam['fovy'], cam['aspect'], cam['zNear'], cam['zFar'])
-
-    GL.glMatrixMode(GL.GL_MODELVIEW)
-
-
-def start(size):
-    GLUT.glutInit(sys.argv)
-
-    GLUT.glutInitDisplayMode(GLUT.GLUT_RGB | GLUT.GLUT_DEPTH)
-    GLUT.glutInitWindowSize(size, size)
-    GLUT.glutInitWindowPosition(0, 0)
-    GLUT.glutCreateWindow('Projections')
-
-    GL.glEnable(GL.GL_DEPTH_TEST)
-    GL.glEnable(GL.GL_TEXTURE_2D)
-    GL.glPixelStorei(GL.GL_PACK_ALIGNMENT, 1)
 
 
 def display(obj, bgcolor, x, y, z, rx, ry, rz):
