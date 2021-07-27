110c289fce8485c00d3106e7d76a7112d97f9294
devansh-dvj <devansh.dvj@gmail.com>
2021-07-28 00:40:38 +0530

    Added test.sh for preview

---
+++ test.sh
@@ -0,0 +1,8 @@
+python src/datagen.py data/Bridge/config.yaml -preview
+python src/datagen.py data/Capetown/config.yaml -preview
+python src/datagen.py data/Fort/config.yaml -preview
+python src/datagen.py data/Map_v1/config.yaml -preview
+python src/datagen.py data/Mine/config.yaml -preview
+python src/datagen.py data/Pumpa/config.yaml -preview
+python src/datagen.py data/Small\ Tropical\ Island/config.yaml -preview
+python src/datagen.py data/Under\ The\ Sea/config.yaml -preview
