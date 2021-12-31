a84c4a4cd1701ae89ec05cd62a9fb2f9f5dae687
devansh-dvj <devansh.dvj@gmail.com>
2022-01-01 00:24:48 +0530

    Completed basic test for project and unproject

--- src/motion_test.py
+++ src/motion_test.py
@@ -27,3 +27,34 @@ def unproject_test(depths, size, modelview, projection, viewport):
 
     return img2obj_map
 
+
+if __name__ == '__main__':
+    np.random.seed(0)
+
+    size = 512
+    depths = np.random.rand(size, size).astype(np.float32)
+    modelview = np.array([[1, 0, 0, 0],
+                          [0, 1, 0, 0],
+                          [0, 0, 1, 0],
+                          [-14.39999962, -1.5, 0, 1]])
+    projection = np.array([[1.73205078, 0, 0, 0],
+                           [0, 1.73205078, 0, 0],
+                           [0, 0, -1.00020003, -1],
+                           [0, 0, -2.00020003, 0]])
+    viewport = np.array([0, 0, size, size]).astype(np.int32)
+
+    img2obj_map = unproject(depths, size, modelview, projection, viewport)
+    img2obj_map_test = unproject_test(depths, size, modelview, projection, viewport)
+    print(img2obj_map.shape, img2obj_map_test.shape)
+    diff = img2obj_map - img2obj_map_test
+    thresh = 10e-10
+    print(np.sum(np.abs(diff) > thresh))
+    print(np.abs(diff).max())
+
+    img2img_map = project(img2obj_map, size, modelview, projection, viewport)
+    img2img_map_test = project_test(img2obj_map_test, size, modelview, projection, viewport)
+    print(img2img_map.shape, img2img_map_test.shape)
+    diff = img2img_map - img2img_map_test
+    thresh = 1
+    print(np.sum(np.abs(diff) > thresh))
+    print(np.abs(diff).max())
