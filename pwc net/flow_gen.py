b7e1189be96e1cf4cd79da51c01b014ff9c641d2
devansh-dvj <devansh.dvj@gmail.com>
2022-03-29 23:33:50 +0530

    Linted flow_gen in pwc net

--- pwc net/flow_gen.py
+++ pwc net/flow_gen.py
@@ -40,10 +40,14 @@ backwarp_tenPartial = {}
 
 def backwarp(tenInput, tenFlow):
     if str(tenFlow.shape) not in backwarp_tenGrid:
-        tenHor = torch.linspace(-1.0 + (1.0 / tenFlow.shape[3]), 1.0 - (
-            1.0 / tenFlow.shape[3]), tenFlow.shape[3]).view(1, 1, 1, -1).expand(-1, -1, tenFlow.shape[2], -1)
-        tenVer = torch.linspace(-1.0 + (1.0 / tenFlow.shape[2]), 1.0 - (
-            1.0 / tenFlow.shape[2]), tenFlow.shape[2]).view(1, 1, -1, 1).expand(-1, -1, -1, tenFlow.shape[3])
+        tenHor = torch.linspace(
+            -1.0 + (1.0 / tenFlow.shape[3]),
+            1.0 - (1.0 / tenFlow.shape[3]),
+            tenFlow.shape[3]).view(1, 1, 1, -1).expand(-1, -1, tenFlow.shape[2], -1)
+        tenVer = torch.linspace(
+            -1.0 + (1.0 / tenFlow.shape[2]),
+            1.0 - (1.0 / tenFlow.shape[2]),
+            tenFlow.shape[2]).view(1, 1, -1, 1).expand(-1, -1, -1, tenFlow.shape[3])
 
         backwarp_tenGrid[str(tenFlow.shape)] = torch.cat([tenHor, tenVer], 1).cuda()
     # end
@@ -57,8 +61,12 @@ def backwarp(tenInput, tenFlow):
                         tenFlow[:, 1:2, :, :] / ((tenInput.shape[2] - 1.0) / 2.0)], 1)
     tenInput = torch.cat([tenInput, backwarp_tenPartial[str(tenFlow.shape)]], 1)
 
-    tenOutput = torch.nn.functional.grid_sample(input=tenInput, grid=(backwarp_tenGrid[str(
-        tenFlow.shape)] + tenFlow).permute(0, 2, 3, 1), mode='bilinear', padding_mode='zeros', align_corners=False)
+    tenOutput = torch.nn.functional.grid_sample(
+        input=tenInput,
+        grid=(backwarp_tenGrid[str(tenFlow.shape)] + tenFlow).permute(0, 2, 3, 1),
+        mode='bilinear',
+        padding_mode='zeros',
+        align_corners=False)
 
     tenMask = tenOutput[:, -1:, :, :]
     tenMask[tenMask > 0.999] = 1.0
@@ -171,10 +179,18 @@ class Network(torch.nn.Module):
 
                 if intLevel < 6:
                     self.netUpflow = torch.nn.ConvTranspose2d(
-                        in_channels=2, out_channels=2, kernel_size=4, stride=2, padding=1)
+                        in_channels=2,
+                        out_channels=2,
+                        kernel_size=4,
+                        stride=2,
+                        padding=1)
                 if intLevel < 6:
                     self.netUpfeat = torch.nn.ConvTranspose2d(
-                        in_channels=intPrevious + 128 + 128 + 96 + 64 + 32, out_channels=2, kernel_size=4, stride=2, padding=1)
+                        in_channels=intPrevious + 128 + 128 + 96 + 64 + 32,
+                        out_channels=2,
+                        kernel_size=4,
+                        stride=2,
+                        padding=1)
                 if intLevel < 6:
                     self.fltBackwarp = [None, None, None, 5.0, 2.5, 1.25, 0.625, None][intLevel + 1]
 
@@ -209,8 +225,8 @@ class Network(torch.nn.Module):
                 )
 
                 self.netSix = torch.nn.Sequential(
-                    torch.nn.Conv2d(in_channels=intCurrent + 128 + 128 + 96 + 64 +
-                                    32, out_channels=2, kernel_size=3, stride=1, padding=1)
+                    torch.nn.Conv2d(in_channels=intCurrent + 128 + 128 + 96 + 64 + 32,
+                                    out_channels=2, kernel_size=3, stride=1, padding=1)
                 )
             # end
 
@@ -222,8 +238,10 @@ class Network(torch.nn.Module):
                     tenFlow = None
                     tenFeat = None
 
-                    tenVolume = torch.nn.functional.leaky_relu(input=correlation.FunctionCorrelation(
-                        tenOne=tenOne, tenTwo=tenTwo), negative_slope=0.1, inplace=False)
+                    tenVolume = torch.nn.functional.leaky_relu(
+                        input=correlation.FunctionCorrelation(tenOne=tenOne, tenTwo=tenTwo),
+                        negative_slope=0.1,
+                        inplace=False)
 
                     tenFeat = torch.cat([tenVolume], 1)
 
@@ -231,8 +249,12 @@ class Network(torch.nn.Module):
                     tenFlow = self.netUpflow(objPrevious['tenFlow'])
                     tenFeat = self.netUpfeat(objPrevious['tenFeat'])
 
-                    tenVolume = torch.nn.functional.leaky_relu(input=correlation.FunctionCorrelation(tenOne=tenOne, tenTwo=backwarp(
-                        tenInput=tenTwo, tenFlow=tenFlow * self.fltBackwarp)), negative_slope=0.1, inplace=False)
+                    tenVolume = torch.nn.functional.leaky_relu(
+                        input=correlation.FunctionCorrelation(
+                            tenOne=tenOne,
+                            tenTwo=backwarp(tenInput=tenTwo, tenFlow=tenFlow * self.fltBackwarp)),
+                        negative_slope=0.1,
+                        inplace=False)
 
                     tenFeat = torch.cat([tenVolume, tenOne, tenFlow, tenFeat], 1)
 
@@ -259,7 +281,8 @@ class Network(torch.nn.Module):
 
                 self.netMain = torch.nn.Sequential(
                     torch.nn.Conv2d(in_channels=81 + 32 + 2 + 2 + 128 + 128 + 96 + 64 + 32,
-                                    out_channels=128, kernel_size=3, stride=1, padding=1, dilation=1),
+                                    out_channels=128, kernel_size=3,
+                                    stride=1, padding=1, dilation=1),
                     torch.nn.LeakyReLU(inplace=False, negative_slope=0.1),
                     torch.nn.Conv2d(in_channels=128, out_channels=128,
                                     kernel_size=3, stride=1, padding=2, dilation=2),
@@ -296,8 +319,13 @@ class Network(torch.nn.Module):
 
         self.netRefiner = Refiner()
 
-        self.load_state_dict({strKey.replace('module', 'net'): tenWeight for strKey, tenWeight in torch.hub.load_state_dict_from_url(
-            url='http://content.sniklaus.com/github/pytorch-pwc/network-' + arguments_strModel + '.pytorch').items()})
+        self.load_state_dict(
+            {strKey.replace('module', 'net'): tenWeight
+             for strKey, tenWeight in torch.hub.load_state_dict_from_url(
+                url='http://content.sniklaus.com/github/pytorch-pwc/network-{}.pytorch'.format(
+                    arguments_strModel),
+            ).items()}
+        )
     # end
 
     def forward(self, tenOne, tenTwo):
